28d809e77a2732c83dcbaacd9a472f9be937db69
iguma <umamaheswari.t@infognana.com>
2013-11-13 14:39:49 +0530

    Second model commit

--- app/models/individual.rb
+++ app/models/individual.rb
@@ -1,6 +1,4 @@
 class Individual < ActiveRecord::Base
-  attr_accessible :current_first_name, :current_last_name
-
   validates :current_personal_title, :format => {:with => /^[a-zA-Z\.]{0,255}$/,allow_blank: true, message: "Only alphabets & dot symbol allowed."}
   validates :current_first_name, :format => {:with => /^[a-zA-Z\. ]{3,255}$/,allow_blank: false, message: "Only alphabets, space & dot symbol allowed."}
   validates :current_middle_name, :format => {:with => /^[a-zA-Z\. ]{0,255}$/,allow_blank: true, message: "Only alphabets, space & dot symbol allowed."}
@@ -11,5 +9,4 @@ class Individual < ActiveRecord::Base
   validates :total_years_work_experience, :format => {:with => /\A[0-9]{0,2}+\z/,allow_blank: true, message: "Invalid work experience. Only numbers allowed."}
   validates :marital_status, :format => {:with => /^[a-zA-Z]{0,1}$/,allow_blank: true, message: "Only alphabets allowed. Maximum length should be 1"}
   validates :social_security_number, :format => {:with => /^\b[0-9]{3}-[0-9]{2}-[0-9]{4}\b/,allow_blank: true, message: "Invalid SSS number. Only numbers & - allowed."}
-
 end
